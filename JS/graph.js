66ce5bbe36385edc440d60fdc359562104085e20
charlottedecau <cd17009@my.bristol.ac.uk>
2018-12-04 18:41:20 +0000

    Initial commit

---
+++ JS/graph.js
@@ -0,0 +1,6 @@
+<input type="button" onClick="location.href='index2.html';" value="About Us"/>
+<script>
+function myFunction() {
+    document.getElementById("panel").style.display = "block";
+}
+</script>
